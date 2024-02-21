105ddd4b37f39ae1eed2870ca4cdc79299ea4642
Reuben Tochukwu Aniekwu <reuben.aniekwu@kellogg.northwestern.edu>
2024-02-21 07:57:12 +0000

    created index page for places

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,2 +1,35 @@
 class PlacesController < ApplicationController
+  def index
+    @place = Place.all
+    # render/show entire list of places on the index page
+  end
+  
+  def show
+    @place = Place.find_by({"id" => params["id"]})
+    # render/show details of a specific place
+  end
+
+  def new
+    @place = Place.new
+    # render contacts/new view with new Contact form
+  end
+
+  def create
+    # start with a new Contact
+    @place = Place.new
+
+    # assign name to a new place (user-entered)
+    @place["name"] = params["name"]
+  
+    # assign relationship between place and journal entry
+    
+    # save Contact row
+    @place.save
+
+    # redirect user if query not found
+    redirect_to "/places/#{@place["id"]}"
+  end
+
+
+
 end
