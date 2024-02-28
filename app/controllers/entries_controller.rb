228d313945daf6eff5100a7792a3988d48eed956
Reuben Tochukwu Aniekwu <reuben.aniekwu@kellogg.northwestern.edu>
2024-02-28 10:09:25 +0000

    My first attempt at hw3_part2

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,2 +1,13 @@
 class EntriesController < ApplicationController
+  def new
+    @entry = Entry.new
+    @entry["place_id"] = params["place_id"]
+  end
+
+  def create
+    @entry = Entry.new
+    @entry["title"] = params["title"]
+    @entry.save
+    redirect_to "/places"
+  end
 end
--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,29 +1,23 @@
 class PlacesController < ApplicationController
   def index
     @places = Place.all
-  
-    # render/show entire list of places on the index page
   end
   
   def show
     @place = Place.find_by({"id" => params["id"]})
-    # render/show details of a specific place
   end
 
   def new
     @place = Place.new
-    # render contacts/new view with new Contact form
   end
 
   def edit
-    # find a Company
     @place = Place.find_by({ "id" => params["id"] })
     @place.save
     redirect_to "/places/#{@place["id"]}"
   end
 
   def update
-    # find a Company
     @place = Place.find_by({ "id" => params["id"] })
     @place.save
     redirect_to "/places/#{@place["id"]}"
